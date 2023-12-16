ff3f01546adb98774893adbc94d952af0c66cf9b
Mahesh Langote <maheshlangote777@gmail.com>
2023-12-16 10:20:47 +0530

    Improved Code '#designed blog simple page'

--- theme/index.js
+++ theme/index.js
@@ -1,29 +1,52 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require("express");
+const app = express();
+const path = require("path");
 
-port = 8080;
-app.listen(port, ()=>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+// Sample data for blog posts
+const blogPosts = [
+  { id: 1, title: "Introduction to Express.js", content: "Lorem ipsum dolor sit amet..." },
+  { id: 2, title: "Building a RESTful API", content: "Lorem ipsum dolor sit amet..." },
+  // Add more sample posts as needed
+];
+
+// Set up EJS as the view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"))
+app.set("views", path.join(__dirname, "/views"));
 
+// Define routes
 
-app.get("/",(req, res)=>{
-    let val = Math.floor(Math.random()*6)+1;
-    res.render("home.ejs", {num:val} )
-    
-})
-app.get("/2", (req,res)=>{
-    res.send("hello")
-})
-app.get("/:username/:id", (req, res)=>{
-    let username = req.params;
-    res.send(`The Username is : ${JSON.stringify(username)}`)
-})
+// Home route to display all blog posts
+app.get("/", (req, res) => {
+  res.render("home.ejs", { posts: blogPosts });
+});
 
-app.get("/search", (req,res)=>{
-    const q = req.query
-    res.send(`the Query is : ${JSON.stringify(q)}`)
-})
+// Route to display a specific blog post
+app.get("/post/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = blogPosts.find((p) => p.id === postId);
+
+  if (post) {
+    res.render("post.ejs", { post });
+  } else {
+    res.status(404).send("Post not found");
+  }
+});
+
+// Route to handle post creation
+app.post("/post/create", (req, res) => {
+  // Implement post creation logic here
+  res.send("Create a new post");
+});
+
+// Search route to search for blog posts
+app.get("/search", (req, res) => {
+  const query = req.query.q;
+  const results = blogPosts.filter((post) => post.title.includes(query) || post.content.includes(query));
+  res.render("search.ejs", { results, query });
+});
+
+// Start the server
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
